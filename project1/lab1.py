fa6d9c2656bcb801dc857c0dbaf7f01f2940c717
MihaiB-dev <mihaibivol.dev@gmail.com>
2023-03-24 17:06:23 +0200

    Finish the DFA

--- project1/lab1.py
+++ project1/lab1.py
@@ -1,22 +1,23 @@
+import sys
 #Pe un rand se citeste tot alfabetul 
-#Pe un rand se citesc toate starile : primul este starea intiala
+#Pe un rand se citesc toate starile : !! primul este starea intiala !!
 # pe randurile urmatoare se citesc drumurile
 #pe ultimul rand se citesc starile finale
 
-with open("nfa.txt","r") as file:
+with open("dfa.txt","r") as file:
     #citim alfabetul si starile
-    alfabet = file.readline().split()
-    stari = file.readline().split()
-
-    #cream DFA-ul
+    alfabet = file.readline().split() # se citeste aflabetul
+    stari = file.readline().split() # se citesc starile
+    cuvant = None
+    #cream automatul
     AUTO = {stare:{litera: [] for litera in alfabet} for stare in stari}
 
     #citim drumurile si punem datele in DFA
     for linie in file.readlines():
         linie = linie.split()
-        if len(linie) == 1 and list(linie[0])[0] in alfabet:
+        if len(linie) == 1 and list(linie[0])[0] in alfabet: #vedem daca este cuvant
             cuvant = linie[0]
-        elif len(linie) != 1 and linie[1] in alfabet :
+        elif len(linie) != 1 and linie[1] in alfabet : #vedem daca sunt drumurile
             AUTO[linie[0]][linie[1]].append(linie[2])
         else:
             for stare in linie:
@@ -24,12 +25,22 @@ with open("nfa.txt","r") as file:
                     break
             else:
                 stare_finala = linie
-    
 
-cuvant = list(cuvant)
 print(AUTO)
-drum = []
-stare_curenta = [stari[0]][0]
+drum = [] # se reface drumul treptat
+stare_curenta = [stari[0]][0] #initializam cu prima stare
+drum.append(stare_curenta)
+
+if cuvant is None: #check if the first node is the final one
+    if stare_curenta in stare_finala:
+        print(f"Input: lambda acceptat, {' '.join(drum)}")
+        sys.exit()
+    else:
+        print("Input : lambda nu este acceptat")
+        sys.exit()
+
+cuvant = list(cuvant) #imparte cuvantul intr-o lista de litere
+
 # print(stare_curenta)
 
 ok = False
